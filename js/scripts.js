a4b505de86fc1056794cee72e956a490d2498277
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-10-20 14:38:47 -0700

    Working on js functions

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function hideResults() {
+  document.getElementById("java").setAttribute("class", "hidden";)
+  document.getElementById("rust").setAttribute("class", "hidden";)
+  document.getElementById("groovy").setAttribute("class", "hidden";)
+}
+
+window.onload = function () {
+  let form = document.querySelector("form#q5");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    hideResults();
+    const q1 = document.querySelector("select#q1").value;
+    const q2 = document.querySelector("input#q2:checked").value;
+    const q3 = document.querySelector("select#q3").value;
+    const q4 = document.querySelector("input#q4:checked").value;
+    const q5 = document.querySelector("select#q5").value;
+
+    if (q1 === "monstera" && q3 === "true" && q4 === "steak" || "sushi") {
+      java.removeAtribute("class");
+    } else if ()
+
+  })
+}
